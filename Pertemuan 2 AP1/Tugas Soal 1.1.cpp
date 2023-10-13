a84ecc103dac1dc887c16dfc3533d2eb3c5e4e40
nainacmla <146154364+nainacmla@users.noreply.github.com>
2023-10-13 18:33:53 +0700

    Add files via upload

---
+++ Pertemuan 2 AP1/Tugas Soal 1.1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int nilaitugas = 80;
+  int nilaiuts = 90;
+  int nilaiuas = 80;
+  float avarage = nilaitugas + nilaiuts + nilaiuas;
+  float nilaiakhir = avarage / 3;
+  cout << nilaiakhir;
+}
---
+++ Pertemuan 2 AP1/Tugas Soal 2.1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+  float v,s,t;
+  cout<<"PROGRAM C++ MENGHITUNG KECEPATAN " <<endl;
+  cout<<"---------------------------------"<<endl;
+  cout<<"masukan jarak tempuh (km)";
+  cin>>s;
+  cout<<"masukan waktu tempuh (jam)";
+  cin>>t;
+  v=s/t;
+  cout<<"kecepatan (km/jam) : " <<v<<endl;
+}
